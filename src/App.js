450c7497d60decb4c8c3593da5f7c0034e7a8154
Premsengar10 <premsengar28@gmail.com>
2023-01-18 23:33:21 +0530

    offset

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import React,{ useEffect } from 'react';
+import ReactGA, { pageview } from 'react-ga'; 
 import HomePage from './Components/HomePage';
 
 function App() {
+  useEffect(() =>{
+    ReactGA.initialize('UA-254528798-1')
+    ReactGA.pageview('window.location.pathname');
+  })
+
   return (
     <div className="App">
       <HomePage />
+      <h2>PRem Seng</h2>
     </div>
   );
 }
